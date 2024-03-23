765590c3a21ff62f5d69090e087574144476047d
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-03-23 13:57:19 +0900

    Add peek

--- src/lexer.rs
+++ src/lexer.rs
@@ -17,20 +17,37 @@ impl<'a> Lexer<'a> {
         let chars = input.chars().peekable();
         Lexer {chars}
     }
+
     pub fn next(self: &mut Self) -> Token {
+        self.skip_ws();
+        if let Some(c) = self.chars.next() {
+            return Lexer::token(c)
+        }
+        Token::EOF
+    }
+
+    pub fn peek(self: &mut Self) -> Token {
+        self.skip_ws();
+        if let Some(c) = self.chars.peek() {
+            return Lexer::token(*c)
+        }
+        Token::EOF
+    }
+
+    pub fn token(c: char) -> Token {
+        match c {
+            '0' ..= '9' |
+            'a' ..= 'z' | 'A' ..= 'Z' => {
+                Token::Atom(c)
+            },
+            _ => Token::Op(c)
+        }
+    }
+
+    pub fn skip_ws(self: &mut Self) {
         while self.chars.peek().map_or(false, |s| s.is_ascii_whitespace()) {
             self.chars.next();
         }
-        if let Some(c) = self.chars.next() {
-            return match c {
-                '0' ..= '9' |
-                'a' ..= 'z' | 'A' ..= 'Z' => {
-                    Token::Atom(c)
-                },
-                _ => Token::Op(c)
-            }
-        }
-        Token::EOF
     }
 }
 
@@ -42,12 +59,14 @@ mod tests {
     fn it_works() {
         let mut l1 = Lexer::new("foo bar");
         
+        assert_eq!(l1.peek(), Token::Atom('f'));
         assert_eq!(l1.next(), Token::Atom('f'));
         assert_eq!(l1.next(), Token::Atom('o'));
         assert_eq!(l1.next(), Token::Atom('o'));
         assert_eq!(l1.next(), Token::Atom('b'));
         assert_eq!(l1.next(), Token::Atom('a'));
         assert_eq!(l1.next(), Token::Atom('r'));
+        assert_eq!(l1.peek(), Token::EOF);
         assert_eq!(l1.next(), Token::EOF);
     }
 
@@ -58,6 +77,7 @@ mod tests {
 
         assert_eq!(l2.next(), Token::Atom('x'));
         assert_eq!(l2.next(), Token::Atom('x'));
+        assert_eq!(l2.peek(), Token::Atom('y'));
         assert_eq!(l2.next(), Token::Atom('y'));
         assert_eq!(l2.next(), Token::Atom('y'));
     }
