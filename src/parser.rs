111ecdd79948cb4c5f2df655c305fae9737cb67f
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-03-23 16:38:34 +0900

    first step

--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,33 @@
 use crate::lexer::*;
 use crate::sexp::*;
 
-pub fn expr(input: &str) -> Box<Sexp> {
+pub fn expr(input: &str) -> Result<Box<Sexp>, String> {
     let mut lexer = Lexer::new(input);
 
     expr_bp(&mut lexer)
 }
 
-fn expr_bp(lexer: &mut Lexer) -> Box<Sexp> {
-    todo!()
+fn expr_bp(lexer: &mut Lexer) -> Result<Box<Sexp>, String> {
+    let lhs = match lexer.next() {
+        Token::Atom(c) => {
+            atom(c.to_string().as_str())
+        },
+        c => {
+            return Err(format!("atom is expected: {:?}", c))
+        }
+    };
+
+    loop {
+        let op = match lexer.next() {
+            Token::EOF => break,
+            Token::Op(c) => c,
+            t => return Err(format!("operator is expected: {:?}", t)),
+        };
+
+        todo!();
+    }
+
+    Ok(lhs)
 }
 
 #[cfg(test)]
@@ -17,6 +36,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(expr("1 + 2 * 3").to_string(), "(+ 1 (* 2 3))")
+        let s = expr("1").unwrap();
+        assert_eq!(s.to_string(), "1");
+        // assert_eq!(expr("1 + 2 * 3").unwrap().to_string(), "(+ 1 (* 2 3))")
     }
 }
